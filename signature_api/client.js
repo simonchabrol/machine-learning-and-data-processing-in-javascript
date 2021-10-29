86732347c350228b35398e125c6fb010c033eb8a
SC <12834916+mylittleneurondotcom@users.noreply.github.com>
2021-10-29 15:33:17 +0200

    Update client.js

--- signature_api/client.js
+++ signature_api/client.js
@@ -1,1 +1,64 @@
+const http = require('http')
+const fs = require('fs')
 
+function Sign() {
+    var Data = JSON.stringify({
+       DocToSign:fs.readFileSync(__dirname+'/Doc.txt').toString('BASE64')
+    })
+    var Options = {
+       host:'localhost',
+       port:'4040',
+       path:'/sign',
+       method:'POST',
+       headers: {
+       'Content-Type':'application/json'
+       }
+    }
+    var req = http.request(Options, function(response) {
+       var RawData=[]
+       response.on('data', function(data) {
+           RawData.push(data)
+       })
+       response.on('end', function() {
+           RawData = JSON.parse(Buffer.concat(RawData).toString())
+           var DocSigned = Buffer.from(RawData.DocSigned, 'base64').toString('utf8')
+           var Signature = Buffer.from(RawData.Signature)
+           fs.writeFileSync('DocSigned.txt', DocSigned, function(err) { throw err })
+           fs.writeFileSync('Signature', Signature, function(err) { throw err })
+       })
+    })
+    req.write(Data)
+    req.end()
+}
+
+Sign() // To sign a document
+
+function Verify() {
+    var Data = JSON.stringify({
+        DocToVerify:fs.readFileSync(__dirname+'/Doc.txt').toString('BASE64'),
+        Signature:fs.readFileSync(__dirname+'/Signature')
+     })
+     var Options = {
+        host:'localhost',
+        port:'4040',
+        path:'/verify',
+        method:'POST',
+        headers: {
+        'Content-Type':'application/json'
+        }
+     }
+     var req = http.request(Options, function(response) {
+        var RawData=[]
+        response.on('data', function(data) {
+            RawData.push(data)
+        })
+        response.on('end', function() {
+            console.log(Buffer.concat(RawData).toString())
+        })
+
+     })
+     req.write(Data)
+     req.end()
+}
+
+Verify() // To verify a signature
