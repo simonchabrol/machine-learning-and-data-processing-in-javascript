45d28ef5c48c036b703a09e81fd54f1216cb1ea8
SC <12834916+mylittleneurondotcom@users.noreply.github.com>
2021-10-30 16:13:28 +0200

    Update main.js

--- shared_hosting/main.js
+++ shared_hosting/main.js
@@ -1,1 +1,24 @@
+var http = require('http')
+var fs = require('fs')
 
+var Conf = JSON.parse(fs.readFileSync('main.json'))
+
+http.createServer(function (request, response) {
+ var Requested = (request.url).split('/')
+  if (Conf[Requested[1]] !== undefined) {
+   var proxyRequest = http.request({
+      method: request.method,
+      host: 'localhost',
+      path: (request.url).replace('/' + Requested[1], ''),
+      body: request.body,
+      port: Conf[Requested[1]]
+    })
+    proxyRequest.on('response', function (proxyResponse) {
+        proxyResponse.pipe(response)
+    })
+    request.pipe(proxyRequest)
+  } 
+    else {
+    response.end('Unable to find app or website : /' + requested[1])
+  }
+}).listen(80)
