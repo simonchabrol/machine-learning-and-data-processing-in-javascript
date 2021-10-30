4a6cbdaa0667338c65c8272b2914e4b86b132cf7
SC <12834916+mylittleneurondotcom@users.noreply.github.com>
2021-10-30 16:13:52 +0200

    Update index.js

--- shared_hosting/API/index.js
+++ shared_hosting/API/index.js
@@ -1,1 +1,15 @@
+var http = require('http')
 
+http.createServer(function (request, response) {
+  if (request.url === '/json' && request.method === 'POST') {
+    var RawData = []
+    request.on('data', function (data) {
+      RawData.push(data)
+    })
+    request.on('end', function() {
+        response.end('Your JSON : ' + RawData)
+    })
+  } else if (request.url === '/' && request.method === 'GET') {
+    response.end('Hello API')
+  }
+}).listen(4040)
