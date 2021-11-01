0ad1041e7a422c8bd81083d9e38ad0a39c79df09
SC <12834916+mylittleneurondotcom@users.noreply.github.com>
2021-11-01 20:38:13 +0100

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,82 +1,64 @@
-var Grid = [  
-   0, 1, 2,
-   3, 4, 5,
-   6, 7, 8
-]
+var Grid = [
+  0, 1, 2,
+  3, 4, 5,
+  6, 7, 8]
 
 var AIMove
 
-function Win (Grid) {
-
-  var WinSituation = [
-     [0,1,2],
-     [3,4,5],
-     [6,7,8],
-
-     [0,3,6],
-     [1,4,7],
-     [2,5,8],
-     
-     [0,4,8],
-     [6,4,2]
-  ]
+function Win(Grid) {
+  var WinSituation = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
 
   for (var i = 0; i < WinSituation.length; i++) {
-
     var TestRow = WinSituation[i]
     var RowValues = []
 
     for (var j = 0; j < TestRow.length; j++) {
-       RowValues.push(Grid[TestRow[j]])
+      RowValues.push(Grid[TestRow[j]])
     }
-
-    var CrossesWin = function (Value) {
-       return Value === 'X'
+    var WinX = function (Value) {
+      return Value === 'X'
     }
-
-    var NoughtsWin = function (Value) {
-       return Value === 'O'
+    var WinO = function (Value) {
+      return Value === 'O'
     }
-
-    if ( RowValues.every(CrossesWin) ) {
+    if (RowValues.every(WinX)) {
       return 1
-    } else if ( RowValues.every(NoughtsWin) ) {
+    } else if (RowValues.every(WinO)) {
       return -1
     }
   }
 }
 
-function Minimax (NewGrid, Player) {
+function Minimax(NewGrid, Player) {
+  var Available = NewGrid.filter(function (a) {
+    return typeof a === 'number'
+  })
+  if (Win(NewGrid) === 1) {
+    return 1
+  }
+  if (Win(NewGrid) === -1) {
+    return -1
+  }
+  if (Available.length === 0) {
+    return 0
+  }
 
-   var Available = NewGrid.filter( function(a) {
-       return typeof number === 'number'
-   })
+  var Moves = []
+  var Scores = []
 
-   if ( Win(NewGrid) === 1 ) {
-      return 1
-   } else if ( Win(NewGrid) === -1 ) {
-      return -1
-   } else if (Available.length === 0) {
-      return 0
-   }
+  for (var i = 0; i < Available.length; i++) {
+    var Move
 
-   var Moves = []
-   var Scores = []
+    Move = NewGrid[Available[i]]
+    NewGrid[Available[i]] = Player
 
-   for (var i = 0; i < Available.length; i++) {
-     var Move
-
-     Move = NewGrid(Available[i])
-
-     NewGrid(Available[i]) = Player
-     
-     if (Player === 'X') {
-       Scores.push(Minimax(NewGrid, 'O'))
-     } else {
-       Scores.push(Minimax(NewGrid, 'X'))
-     }
-     NewGrid[Available[i]] = Move
-     Moves.push(Move)
+    if (Player === 'X') {
+      Scores.push(Minimax(NewGrid, 'O'))
+    } else {
+      Scores.push(Minimax(NewGrid, 'X'))
+    }
+    NewGrid[Available[i]] = Move
+    Moves.push(Move)
   }
 
   if (Player === 'X') {
@@ -100,9 +82,7 @@ while (Play !== 0) {
   var Available = Grid.filter(function (a) {
     return typeof a === 'number'
   })
-  if ( (Win(Grid) === 1)  || 
-       (Win(Grid) === -1) || 
-       (Available.length === 0) ) {
+  if ((Win(Grid) === 1) || (Win(Grid) === -1) || (Available.length === 0)) {
     Play = 0
   } else {
     if (Turn === 1) {
@@ -113,11 +93,11 @@ while (Play !== 0) {
       Turn = 1
     }
     var Choice = Minimax(Grid, AI)
-    Grid[Choice.Position] = AI
+    Grid[AIMove] = AI
     console.log('\n')
-    console.log(Grid[0],Grid[1],Grid[2])
-    console.log(Grid[3],Grid[4],Grid[5])
-    console.log(Grid[6],Grid[7],Grid[8])
+    console.log(Grid[0], Grid[1], Grid[2])
+    console.log(Grid[3], Grid[4], Grid[5])
+    console.log(Grid[6], Grid[7], Grid[8])
   }
 }
 
