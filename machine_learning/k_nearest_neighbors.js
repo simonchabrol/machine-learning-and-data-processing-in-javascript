3a00e3c7be2a8ee107e484d2825d549e05ae4802
SC <12834916+mylittleneurondotcom@users.noreply.github.com>
2021-11-01 13:42:36 +0100

    Create k_nearest_neighbors.js

---
+++ machine_learning/k_nearest_neighbors.js
@@ -0,0 +1,94 @@
+var Input = [
+    [0,0],
+    [1,0],
+    [0,1],
+    [1,1]
+]
+
+var Output = [
+    0,
+    1,
+    1,
+    0
+]
+
+var TestInput = [
+    [0,0.25], // Expected 0
+    [0.25,0], // Expected 0
+    [0.25,1], // Expected 1
+    [1,0.25], // Expected 1
+    [0.75,1]  // Expected 0
+]
+
+var kNearest = 1
+// var kNearest = Math.sqrt(Input.length)
+
+console.log('k : ' + kNearest)
+
+for (var i = 0; i < TestInput.length; i++) {
+    var TestValue = TestInput[i]
+    var Counting = []
+    for (var j = 0; j < Input.length; j++) {
+      var TestedInput = Input[j]
+      var Count = 0
+      for (var k = 0; k < TestValue.length; k++) {
+        Count = Count + Math.pow(TestValue[k] - TestedInput[k],2)
+      }
+      Counting.push(Math.sqrt(Count))
+    }
+
+    Array.min = function(array, kNearest ){
+       return array.sort(function(a, b){
+            return a-b
+       }).slice(0, kNearest)
+    }
+
+   var CountingCopy = [...Counting]
+   var CountList = [...new Set(Array.min(CountingCopy,kNearest))]
+
+    var Results = []
+    for (var l = 0; l < CountList.length; l++) {
+       for (var m = 0; m < Counting.length; m++) {
+        if (CountList[l] === Counting[m]) {
+          Results.push(Output[m])
+        }
+      } 
+    }
+    
+    var AllResults = [
+      {'Class': 0, 'Result': 0},
+      {'Class': 1, 'Result': 0}
+    ]
+    
+    for (var m = 0; m < Results.length; m++) {
+      for (var n = 0; n < AllResults.length; n++) {
+        if (Results[m] === AllResults[n].Class) {
+          AllResults[n].Result += 1
+        }
+      }
+    }
+    
+    var Max = AllResults.reduce(function (Prev, Current) {
+      if (Prev.Result > Current.Result) {
+        return Prev
+      } else {
+        return Current
+      }
+    })
+    
+    var Unique = 0
+    var FinalValue = 0
+
+    for (var o = 0; o < AllResults.length; o++) {
+      if (AllResults[o].Result == Max.Result) {
+        Unique += 1
+      }
+    }
+    if (Unique === 1) {
+      FinalValue = Max.Class
+    } else {
+      FinalValue = 'No class'
+    }
+    
+    console.log(TestValue,FinalValue)
+}
