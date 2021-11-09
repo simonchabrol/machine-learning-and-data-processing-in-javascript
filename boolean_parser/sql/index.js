5e3a14cf1c72479dec6fb7ff4d6aab0bf49079ba
Simon CHABROL <12834916+mylittleneurondotcom@users.noreply.github.com>
2021-11-09 15:44:43 +0100

    Update index.js

--- boolean_parser/sql/index.js
+++ boolean_parser/sql/index.js
@@ -1,123 +1,200 @@
 var ARR = [
-    ['id', 'first', 'last'],
-    [1, 2, 2],
-    [1, 2, 3],
-    [1, 1, 4],
-    [2, 1, 2],
-    [2, 2, 1]]
+  ['id', 'first', 'last'],
+  [1, 2, 2],
+  [1, 2, 3],
+  [1, 1, 4],
+  [2, 1, 2],
+  [2, 2, 1]
+]
 
 var EXPList = [
-    'id=1 OR first=2 AND last=4 OR last=3', // Expected : [ 1, 2, 2 ], [ 1, 2, 3 ], [ 1, 1, 4 ]
-    'id=1 OR id=2 AND last=4', // Expected : [ 1, 2, 2 ], [ 1, 2, 3 ], [ 1, 1, 4 ]
-    'first=2 AND last=1', // Expected : [ 2, 2, 1 ]
-    'first=2', // Expected : [ 1, 2, 2 ], [ 1, 2, 3 ], [ 2, 2, 1 ]
-    'last=3', // [ 1, 2, 3 ]
-    'id=1 OR id=2 AND last=2 OR last=3', // [ 1, 2, 2 ], [ 1, 2, 3 ], [ 1, 1, 4 ], [ 2, 1, 2 ]
-    'id=1 AND first>1', // Expected : [ 1, 2, 2 ], [ 1, 2, 3 ]
-    'last<3', // Expected : [ 1, 2, 2 ], [ 2, 1, 2 ], [ 2, 2, 1 ]
-    'id>=1 AND last>=2 AND last<3', // Expected : [ 1, 2, 2 ], [ 2, 1, 2 ]
-    'id!=2 AND last!=2' // Expected : [ 1, 2, 3 ], [ 1, 1, 4 ]
+  'id=1 OR first=2 AND last=4 OR last=3',
+  '( id=1 AND ( first=2 ) ) AND ( last=4 OR last=3 )',
+  'id=1 OR id=2 AND last=4',
+  'first=2 AND last=1',
+  'first=2',
+  'last=3',
+  'id=1 OR id=2 AND last=2 OR last=3',
+  'id=1 AND first>1',
+  'last<3',
+  '( id=1 AND ( first>1 AND last<3 ) ) OR ( id=2 AND ( first=2 AND last=1 ) )',
+  'id>=1 AND last>=2 AND last<3',
+  'id!=2 AND last!=2',
+  'id=1 AND ( first=2 OR last=2 )'
 ]
 
 for (var k = 0; k < EXPList.length; k++) {
 
-    console.log(EXPList[k])
+  console.log(EXPList[k])
 
-    var RawEXP = EXPList[k].split(' ')
+  var RawEXP = EXPList[k].split(' ')
 
-    var EXP = []
+  var EXP = []
 
-    for (var i = 0; i < RawEXP.length; i++) {
-        if (RawEXP[i] === 'OR' || RawEXP[i] === 'AND') {
-            if (i !== 0 && i !== RawEXP.length) {
-                EXP.push(RawEXP[i])
-            }
+  for (var i = 0; i < RawEXP.length; i++) {
+    if (RawEXP[i] === 'OR' || RawEXP[i] === 'AND' && RawEXP[i] !== '(' && RawEXP[i] !== ')') {
+      if (i !== 0 && i !== RawEXP.length) {
+        EXP.push(RawEXP[i])
+      }
+    } else if (RawEXP[i] === '(' || RawEXP[i] === ')') {
+      EXP.push(RawEXP[i])
+    } else {
+      var Regex = /(>=|<=|<|>|=|!=)/i
+      var Value = RawEXP[i].split(Regex)
+      if (ARR[0].indexOf(Value[0]) !== -1) {
+        EXP.push([ARR[0].indexOf(Value[0]), Value[2], Value[1]])
+      }
+    }
+  }
+
+  function Check(Arr, Condition) {
+    switch (Condition[2]) {
+      case '=':
+        if (Arr[Condition[0]] == Condition[1]) {
+          return 1
         } else {
-            var Regex = /(>=|<=|<|>|=|!=)/i
-            var Value = RawEXP[i].split(Regex)
-            if (ARR[0].indexOf(Value[0]) !== -1) {
-                EXP.push([ARR[0].indexOf(Value[0]), Value[2], Value[1]])
-            }
+          return 0
+        }
+      case '>=':
+        if (Arr[Condition[0]] >= Condition[1]) {
+          return 1
+        } else {
+          return 0
+        }
+      case '>':
+        if (Arr[Condition[0]] > Condition[1]) {
+          return 1
+        } else {
+          return 0
+        }
+      case '<=':
+        if (Arr[Condition[0]] <= Condition[1]) {
+          return 1
+        } else {
+          return 0
+        }
+      case '<':
+        if (Arr[Condition[0]] < Condition[1]) {
+          return 1
+        } else {
+          return 0
+        }
+      case '!=':
+        if (Arr[Condition[0]] != Condition[1]) {
+          return 1
+        } else {
+          return 0
         }
     }
+  }
 
-    function Check(Arr, Condition) {
-        switch (Condition[2]) {
-            case '=':
-                if (Arr[Condition[0]] == Condition[1]) {
-                    return 1
-                } else {
-                    return 0
-                }
-            case '>=':
-                if (Arr[Condition[0]] >= Condition[1]) {
-                    return 1
-                } else {
-                    return 0
-                }
-            case '>':
-                if (Arr[Condition[0]] > Condition[1]) {
-                    return 1
-                } else {
-                    return 0
-                }
-            case '<=':
-                if (Arr[Condition[0]] <= Condition[1]) {
-                    return 1
-                } else {
-                    return 0
-                }
-            case '<':
-                if (Arr[Condition[0]] < Condition[1]) {
-                    return 1
-                } else {
-                    return 0
-                }
-            case '!=':
-                if (Arr[Condition[0]] != Condition[1]) {
-                    return 1
-                } else {
-                    return 0
-                }
-        }
+  for (var i = 1; i < ARR.length; i++) {
+    var Operators = []
+    for (var j = 0; j < EXP.length; j++) {
+      if (EXP[j] !== 'AND' && EXP[j] !== 'OR' && EXP[j] !== ')' && EXP[j] !== '(') {
+        Operators.push(Check(ARR[i], EXP[j]))
+      }
+      if (EXP[j] === ')') {
+        Operators.push(')')
+      }
+      if (EXP[j] === '(') {
+        Operators.push('(')
+      }
+      if (EXP[j + 1] === 'AND') {
+        Operators.push('&&')
+      }
+      if (EXP[j + 1] === 'OR') {
+        Operators.push('||')
+      }
     }
 
-    for (var i = 1; i < ARR.length; i++) {
-        var Operators = []
-        for (var j = 0; j < EXP.length; j++) {
-            if (EXP[j] !== 'AND' && EXP[j] !== 'OR') {
-                Operators.push(Check(ARR[i], EXP[j]))
+    var OpenParenthesis = []
+    var ClosedParenthesis = []
+
+    function CheckParenthesis() {
+      OpenParenthesis = []
+      ClosedParenthesis = []
+      for (var i = 0; i < Operators.length; i++) {
+        if (Operators[i] === '(') {
+          for (var j = i + 1; j < Operators.length; j++) {
+            if (Operators[j] === ')') {
+              OpenParenthesis.push(i)
+              ClosedParenthesis.push(j)
+              break
+            } else if (Operators[j] === '(') {
+              break
             }
-            if (EXP[j + 1] === 'AND') {
-                Operators.push('&&')
-            }
-            if (EXP[j + 1] === 'OR') {
-                Operators.push('||')
-            }
+          }
         }
-        for (var j = 0; j < Operators.length; j++) {
-            if (Operators[j] === '&&') {
-                Operators[j - 1] = Operators[j - 1] && Operators[j + 1]
-                Operators.splice(j, 1)
-                Operators.splice(j, 1)
-                j = 0
-            }
+      }
+    }
+
+    CheckParenthesis()
+
+    if (OpenParenthesis.length !== 0) {
+      for (var l = 0; l < OpenParenthesis.length; l++) {
+        for (var j = OpenParenthesis[l]; j < ClosedParenthesis[l]; j++) {
+          if (Operators[j] === '&&' && Operators[j + 1] !== '(' && Operators[j + 1] !== ')') {
+            Operators[j - 1] = Operators[j - 1] && Operators[j + 1]
+            Operators.splice(j, 1)
+            Operators.splice(j, 1)
+            j = OpenParenthesis[l]
+            CheckParenthesis()
+          }
+          if (OpenParenthesis[l] - ClosedParenthesis[l] === -2) {
+            Operators.splice(OpenParenthesis[l], 1)
+            Operators.splice(ClosedParenthesis[l] - 1, 1)
+            OpenParenthesis.splice(l, 1)
+            ClosedParenthesis.splice(l, 1)
+            CheckParenthesis()
+          }
         }
-        for (var j = 0; j < Operators.length; j++) {
-            if (Operators[j] === '||') {
-                Operators[j - 1] = Operators[j - 1] || Operators[j + 1]
-                Operators.splice(j, 1)
-                Operators.splice(j, 1)
-                j = 0
-            }
+        for (var j = OpenParenthesis[l]; j < ClosedParenthesis[l]; j++) {
+          if (Operators[j] === '||' && Operators[j + 1] !== '(' && Operators[j + 1] !== ')') {
+            Operators[j - 1] = Operators[j - 1] || Operators[j + 1]
+            Operators.splice(j, 1)
+            Operators.splice(j, 1)
+            j = OpenParenthesis[l]
+            CheckParenthesis()
+          }
+          if (OpenParenthesis[l] - ClosedParenthesis[l] === -2) {
+            Operators.splice(OpenParenthesis[l], 1)
+            Operators.splice(ClosedParenthesis[l] - 1, 1)
+            OpenParenthesis.splice(l, 1)
+            ClosedParenthesis.splice(l, 1)
+            CheckParenthesis()
+          }
         }
 
-        var Test = Operators[0]
-        if (Test === 1) {
-            console.log(ARR[i])
+        if (OpenParenthesis.length !== 0) {
+          l--
         }
+      }
     }
-    console.log('\r')
+
+    for (var j = 0; j < Operators.length; j++) {
+      if (Operators[j] === '&&') {
+        Operators[j - 1] = Operators[j - 1] && Operators[j + 1]
+        Operators.splice(j, 1)
+        Operators.splice(j, 1)
+        j = 0
+      }
+    }
+    for (var j = 0; j < Operators.length; j++) {
+      if (Operators[j] === '||') {
+        Operators[j - 1] = Operators[j - 1] || Operators[j + 1]
+        Operators.splice(j, 1)
+        Operators.splice(j, 1)
+        j = 0
+      }
+    }
+
+    var Test = Operators[0]
+    if (Test === 1) {
+      console.log(ARR[i])
+    }
+  }
+  console.log('\r')
 }
 
 /*
@@ -125,6 +202,9 @@ id=1 OR first=2 AND last=4 OR last=3
 [ 1, 2, 2 ]
 [ 1, 2, 3 ]
 [ 1, 1, 4 ]
+
+( id=1 AND ( first=2 ) ) AND ( last=4 OR last=3 )
+[ 1, 2, 3 ]
 
 id=1 OR id=2 AND last=4
 [ 1, 2, 2 ]
@@ -157,6 +237,10 @@ last<3
 [ 2, 1, 2 ]
 [ 2, 2, 1 ]
 
+( id=1 AND ( first>1 AND last<3 ) ) OR ( id=2 AND ( first=2 AND last=1 ) )
+[ 1, 2, 2 ]
+[ 2, 2, 1 ]
+
 id>=1 AND last>=2 AND last<3
 [ 1, 2, 2 ]
 [ 2, 1, 2 ]
@@ -164,4 +248,8 @@ id>=1 AND last>=2 AND last<3
 id!=2 AND last!=2
 [ 1, 2, 3 ]
 [ 1, 1, 4 ]
+
+id=1 AND ( first=2 OR last=2 )
+[ 1, 2, 2 ]
+[ 1, 2, 3 ]
 */
