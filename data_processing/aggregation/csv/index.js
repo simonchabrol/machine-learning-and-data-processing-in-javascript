595c4949416989d6ded47e7a0dc626614c7c1609
Simon CHABROL <12834916+mylittleneurondotcom@users.noreply.github.com>
2021-11-12 18:34:36 +0100

    Update index.js

--- data_processing/aggregation/csv/index.js
+++ data_processing/aggregation/csv/index.js
@@ -14,7 +14,7 @@ function Process(File, PreviousRevenue) {
     var Sum = 0
 
     lineReader.on('line', function (line) {
-        console.log('Line processed : ' + line)
+        console.log('Line : ' + line)
         var Line = line.split(';')
         if (Line.includes('revenue')) {
             Index = Line.indexOf('revenue')
@@ -42,35 +42,34 @@ function Result(Sum) {
 Process(Folder[i], 0, Result)
 
 /*
-Line processed : id;name;revenue;country
-Line processed : 1;A;100;FR
-Line processed : 2;B;200;FR
-Line processed : 3;C;200;DE
-Line processed : 4;D;300;GB
-Line processed : 5;E;300;DE
-Line processed : 6;F;400;IT
-Line processed : 7;G;200;FR
-Line processed : 8;H;100;FR
+Line : id;name;revenue;country
+Line : 1;A;100;FR
+Line : 2;B;200;FR
+Line : 3;C;200;DE
+Line : 4;D;300;GB
+Line : 5;E;300;DE
+Line : 6;F;400;IT
+Line : 7;G;200;FR
+Line : 8;H;100;FR
 ./files/data_1.csv revenue is : 1800
-Line processed : id;name;country;revenue
-Line processed : 1;A;FR;100
-Line processed : 2;B;FR;200
-Line processed : 3;C;DE;200
-Line processed : 4;D;GB;300
-Line processed : 5;E;DE;300
-Line processed : 6;F;IT;400
-Line processed : 7;G;FR;200
-Line processed : 8;H;FR;100
+Line : id;name;country;revenue
+Line : 1;A;FR;100
+Line : 2;B;FR;200
+Line : 3;C;DE;200
+Line : 4;D;GB;300
+Line : 5;E;DE;300
+Line : 6;F;IT;400
+Line : 7;G;FR;200
+Line : 8;H;FR;100
 ./files/data_2.csv revenue is : 1800
-Line processed : id;revenue;country;name
-Line processed : 1;100;FR;A
-Line processed : 2;200;FR;B
-Line processed : 3;200;DE;C
-Line processed : 4;300;GB;D
-Line processed : 5;300;DE;E
-Line processed : 6;400;IT;F
-Line processed : 7;200;FR;G
-Line processed : 8;100;FR;H
+Line : id;revenue;country;name
+Line : 1;100;FR;A
+Line : 2;200;FR;B
+Line : 3;200;DE;C
+Line : 4;300;GB;D
+Line : 5;300;DE;E
+Line : 6;400;IT;F
+Line : 7;200;FR;G
+Line : 8;100;FR;H
 ./files/data_3.csv revenue is : 1800
-Aggregated revenue is : 5400
 */
