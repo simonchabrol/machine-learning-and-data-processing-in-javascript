c48000f9f16434df04855227c0c1575f584d8898
Simon CHABROL <12834916+mylittleneurondotcom@users.noreply.github.com>
2021-11-12 14:28:53 +0100

    Update index.js

--- data_processing/aggregation/json/index.js
+++ data_processing/aggregation/json/index.js
@@ -1,1 +1,70 @@
+const fs = require('fs');
+const readline = require('readline')
 
+var Folder = fs.readdirSync('./files/')
+var i = 0
+
+function Process(File, PreviousRevenue) {
+
+    var lineReader = readline.createInterface({
+        input: fs.createReadStream('./files/data_1.json')
+    })
+
+    var Sum = 0
+
+    lineReader.on('line', function (line) {
+        if (line.match('revenue') !== null) {
+            function ProcessLine() {
+                line = line.trim()
+                line = line.trimStart()
+                line = line.replace(/}/g, '')
+                line = line.replace(/{/g, '')
+                if (line[0] === ',') {
+                    line = line.slice(0)
+                }
+                if (line[line.length - 1] === ',') {
+                    line = line.slice(0, -1)
+                }
+                line = '{' + line + '}'
+            }
+            ProcessLine()
+            while (line.indexOf('revenue') !== -1) {
+                var json = JSON.parse(line)
+                console.log('Revenue found : ' + json.revenue)
+                Sum += json.revenue
+                line = line.slice(0, line.lastIndexOf('revenue') - 1)
+                ProcessLine()
+            }
+        }
+    })
+
+    lineReader.on('close', function () {
+        console.log('./files/' + File + ' revenue is : ' + Sum)
+        Result(Sum+PreviousRevenue)
+    })
+
+}
+
+function Result(Sum) {
+    if (i + 1 < Folder.length) {
+        i++
+        Process(Folder[i], Sum, Result)
+    } else {
+        console.log('Aggregated revenue is : ' + Sum)
+    }
+}
+
+Process(Folder[i], 0, Result)
+
+/*
+Revenue found : 100
+Revenue found : 200
+Revenue found : 200
+Revenue found : 300
+Revenue found : 200
+Revenue found : 400
+Revenue found : 300
+Revenue found : 100
+./files/data_1.json revenue is : 1800
+Aggregated revenue is : 1800
+*/
