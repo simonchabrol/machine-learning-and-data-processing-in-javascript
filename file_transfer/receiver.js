16451fe4ceb542d1e89cab8abb34e017bf008263
Simon CHABROL <12834916+mylittleneurondotcom@users.noreply.github.com>
2022-02-26 17:45:58 +0100

    Update receiver.js

--- file_transfer/receiver.js
+++ file_transfer/receiver.js
@@ -1,39 +1,53 @@
-var http = require('http')
+var http = require("http")
 var fs = require("fs")
+var readline = require("readline")
 
 http.createServer(function (req, res) {
-  if (req.method === 'POST' && req.url === '/') {
-    console.log('Device ready for transfer')
-    var RawData=[]
-    req.on('data', function(data) {
-         RawData.push(data)
-    })
-    req.on('end', function() {
+    if (req.method === "POST" && req.url === "/") {
+      var RawData = []
+      req.on("data", function (data) {
+        RawData.push(data)
+      })
+      req.on("end", function () {
         var Json = JSON.parse(Buffer.concat(RawData).toString())
         var Host = Json.Host
-        var options = {
-          hostname: Host,
-          port: 8080,
-          path: '/',
-          method: 'GET'
-        }
-        
-        var request = http.request(options, function(res)  {
-          console.log('Incoming data...')
-          var fileStream = fs.createWriteStream('NewTextFile.txt')
-          res.on('data', function(data) {
-            fileStream.write(data)
-            console.log(data)
-          })
-          res.on('end', function() {
-             console.log('File downloaded')
-          })
+        var Code = Json.Code
+        var rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout,
         })
-        request.on('error', function(error) {
-          console.error('An error occured : ' + error)
+        rl.question("This code " + Code + " ? (Y)", function (response) {
+          if (response.toUpperCase() === "Y") {
+            console.log("Device ready for transfer")
+            var options = {
+              hostname: Host,
+              port: 8080,
+              path: "/",
+              method: "GET",
+            }
+
+            var request = http.request(options, function (res) {
+              console.log("Incoming data...")
+              var fileStream = fs.createWriteStream("NewTextFile.txt");
+              res.on("data", function (data) {
+                fileStream.write(data)
+                console.log(data)
+              })
+              res.on("end", function () {
+                console.log("File transfer is done")
+              })
+            })
+            request.on("error", function (error) {
+              console.error(error)
+            });
+            request.end()
+            res.end()
+          } else {
+            res.setTimeout(1)
+            rl.close()
+          }
         })
-        request.end()
-        res.end()
-    })
-  } 
+        rl.on("close", function () {})
+      })
+    }
 }).listen(8085)
