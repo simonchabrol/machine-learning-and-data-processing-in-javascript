41c5bd239b75bdac25d03ef416ec3e1a5ffc5df6
Simon CHABROL <12834916+mylittleneurondotcom@users.noreply.github.com>
2022-12-11 11:42:05 +0100

    Update markov_chain.js

--- machine_learning/markov_chain.js
+++ machine_learning/markov_chain.js
@@ -1,3 +1,101 @@
+// First order
+
+var Sentences = [
+    'this new humans of new-york post is gross and somewhat terrifying',
+    'artificial intelligence cant think without polluting',
+    'a massive asteroid collision once caused earth to cool and enter an ice age',
+    'venus was potentially habitable until a mysterious event happened',
+    'china releases new images of bizzare substance found in a moon crater',
+    'venus could have supported life for billions of years',
+    'how to predict crucial plasma pressure in future fusion facilities',
+    'undeground continents may be as old as earth',
+    'washington monument elevator breaks down days after landmark reopens',
+    'anti-ice demonstrators and counter-protesters face off in aurora colorado',
+    'police search for a suspect after 6 people are shot in downtown indianapolis',
+    'hong-kong riots police curb airport protest after clashes',
+    'the general motors strike will test which party will work for workers in 2020',
+    'fed injects cash for fourth day as funding markets stabilize',
+    'funeral held for legendary journalist today',
+    'woman arrested after allegedly kicking dog in viral video'
+]
+
+
+var UniqueWords = []
+
+for (var i = 0; i < Sentences.length; i++) {
+    UniqueWords.push(...new Set(Sentences[i].split(' ')))
+    UniqueWords = [...new Set(UniqueWords)]
+    Sentences[i] = Sentences[i].split(' ')
+}
+
+var ProbabilityMatrix = []
+
+for (var i = 0; i < UniqueWords.length; i++) {
+    ProbabilityMatrix.push([])
+    for (var j = 0; j < UniqueWords.length; j++) {
+        ProbabilityMatrix[i].push(0)
+    }
+}
+
+for (var i = 0; i < UniqueWords.length; i++) {
+    for (var j = 0; j < UniqueWords.length; j++) {
+        for (var k = 0; k < Sentences.length; k++) {
+            var Index = Sentences[k].indexOf(UniqueWords[i])
+            if (Index !== -1) {
+                if (Sentences[k][Index + 1] === UniqueWords[j]) {
+                    ProbabilityMatrix[i][j] += 1
+                }
+            }
+        }
+    }
+}
+
+for (var i = 0; i < UniqueWords.length; i++) {
+    for (var j = 0; j < UniqueWords.length; j++) {
+        ProbabilityMatrix[i][j] /= Sentences.length
+    }
+}
+
+var FirstWord = 'search'
+var NewSentence = ''
+for (var i = UniqueWords.indexOf(FirstWord); i < UniqueWords.length; i++) {
+    var Results = []
+    var Choices = []
+    i = UniqueWords.indexOf(FirstWord)
+    if (i + 1 < UniqueWords.length && i !== -1) {
+        for (var j = 0; j < UniqueWords.length; j++) {
+            if (ProbabilityMatrix[i][j] !== 0) {
+                Results.push(ProbabilityMatrix[i][j])
+                Choices.push(UniqueWords[j])
+            }
+        }
+        if (Results.length !== 0) {
+            var Max = Math.max(...Results)
+            if (Results.indexOf(Max) === Results.lastIndexOf(Max)) {
+                NewSentence = NewSentence.concat(' ' + FirstWord)
+                FirstWord = Choices[Results.indexOf(Max)]
+            } else {
+                var Take = -1
+                while (Results[Take] !== Max) {
+                    Take = Math.floor(Math.random() * (Results.length - 0) + 0)
+                }
+                NewSentence = NewSentence.concat(' ' + FirstWord)
+                FirstWord = Choices[Take]
+            }
+        } else {
+            NewSentence = NewSentence.concat(' ' + FirstWord)
+            break
+        }
+    } else {
+        NewSentence = NewSentence.concat(' ' + FirstWord)
+        break
+    }
+}
+
+console.log(NewSentence)
+
+// N order (at least 2)
+
 var Sentences = [
     'this new humans of new-york post is gross and somewhat terrifying',
     'artificial intelligence cant think without polluting',
