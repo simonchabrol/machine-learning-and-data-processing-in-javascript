5d004e2afa08d20feb066381d587fa93b5f5a3af
Simon CHABROL <12834916+mylittleneurondotcom@users.noreply.github.com>
2023-01-05 20:06:16 +0100

    Update sort_algorithms.js

--- data_processing/sort_algorithms.js
+++ data_processing/sort_algorithms.js
@@ -2,30 +2,16 @@
   Bubble Sort
 */
 
-var ToSort = [4,2,5,1,3]
-// ToSort = [[2,1],[1,1],[2,3],[2,2],[0,1]]
+var ToSort = [4,2,5,1,3,6]
 
 var Swap = 0
 
 for (var i = 0; i < ToSort.length-1; i++) {
-  if (ToSort[0].length !== undefined) {
-     for (var j = 0; j < ToSort[i].length; j++) {
-       if (ToSort[i+1][j] < ToSort[i][j]) {
-         var Temp = [ToSort[i+1][j],ToSort[i][j]]
-         ToSort[i+1][j] = Temp[1]
-         ToSort[i][j] = Temp[0]
-         Swap += 1
-       }
-     }
-   }
-  
-  else {
-     if (ToSort[i+1] < ToSort[i]) {
+   if (ToSort[i+1] < ToSort[i]) {
         Temp = [ToSort[i+1],ToSort[i]]
         ToSort[i+1] = Temp[1]
         ToSort[i] = Temp[0]
         Swap += 1
-     }
    }
 
    if (i+1 === ToSort.length-1) {
@@ -41,16 +27,14 @@ for (var i = 0; i < ToSort.length-1; i++) {
 console.log(ToSort)
 
 /*
-[ 1, 2, 3, 4, 5 ]
-[ [ 0, 1 ], [ 1, 1 ], [ 2, 1 ], [ 2, 2 ], [ 2, 3 ] ]
+[ 1, 2, 3, 4, 5, 6 ]
 */
 
 /*
   Comb Sort
 */
 
-var ToSort = [4,2,5,1,3]
-// ToSort = [[2,1],[1,1],[2,3],[2,2],[0,1]]
+var ToSort = [4,2,5,1,3,6]
 
 var ToSortRange = ToSort.length
 while (ToSortRange > 1 || Swap === 1) {
@@ -61,22 +45,11 @@ while (ToSortRange > 1 || Swap === 1) {
    var i = 0
    var Swap = 0
    while(i < ToSort.length - ToSortRange) {
-      if (ToSort[0].length !== undefined) {
-        for (var j = 0; j < ToSort[0].length; j++) {
-          if (ToSort[i][j] > ToSort[i + ToSortRange][j]) {
-            var Temp = [ToSort[i][j],ToSort[i + ToSortRange][j]]
-            ToSort[i][j] = Temp[1]
-            ToSort[i + ToSortRange][j] = Temp[0]
-            Swap = 1
-          }
-        }
-      } else {
-        if (ToSort[i] > ToSort[i + ToSortRange]) {
-           Temp = [ToSort[i],ToSort[i + ToSortRange]]
-           ToSort[i] = Temp[1]
-           ToSort[i + ToSortRange] = Temp[0]
-           Swap = 1
-        }
+      if (ToSort[i] > ToSort[i + ToSortRange]) {
+         Temp = [ToSort[i],ToSort[i + ToSortRange]]
+         ToSort[i] = Temp[1]
+         ToSort[i + ToSortRange] = Temp[0]
+         Swap = 1
       }
       i += 1
    }
@@ -85,12 +58,10 @@ while (ToSortRange > 1 || Swap === 1) {
 console.log(ToSort)
 
 /*
-[ 1, 2, 3, 4, 5 ]
-[ [ 0, 1 ], [ 1, 1 ], [ 2, 1 ], [ 2, 2 ], [ 2, 3 ] ]
+[ 1, 2, 3, 4, 5, 6 ]
 */
 
-var ToSort = [4,2,5,1,3]
-// ToSort = [[2,1],[1,1],[2,3],[2,2],[0,1]]
+var ToSort = [4,2,5,6,1,3]
 
 function QuickSort (List,Low,High) {
 
@@ -104,7 +75,6 @@ function QuickSort (List,Low,High) {
 }
 
 function Partition (List,Low,High) {
-  if (List[0].length === undefined) {
     var Pivot = List[High]
     var Index = Low
     var Temp
@@ -120,33 +90,11 @@ function Partition (List,Low,High) {
     List[Index] = Temp[1]
     List[High] = Temp[0]
     return Index
-  }
-  
-   else {
-     Pivot = List[High]
-     Index = Low
-     Temp
-     for (var j = Low; j < High; j++) {
-       for (var k = 0; k < List[0].length; k++) {
-         if (List[j][k] < Pivot[k]) {
-           Temp = [List[Index],List[j]]
-           List[Index] = Temp[1]
-           List[j] = Temp[0]
-           Index += 1
-         }
-       }
-     }
-     Temp = [List[Index],List[High]]
-     List[Index] = Temp[1]
-     List[High] = Temp[0]
-     return Index
-   } 
 }
 
 QuickSort(ToSort,0,ToSort.length-1)
 console.log(ToSort)
 
 /*
-[ 1, 2, 3, 4, 5 ]
-[ [ 0, 1 ], [ 1, 1 ], [ 2, 1 ], [ 2, 2 ], [ 2, 3 ] ]
+[ 1, 2, 3, 4, 5, 6 ]
 */
