6fb81b837a2959f43eb9ccea3bbb109ad1c1d19e
Simon CHABROL <12834916+mylittleneurondotcom@users.noreply.github.com>
2023-01-18 20:20:38 +0100

    Update server.js

--- auth_api/server.js
+++ auth_api/server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const cryto = require('crypto')
+const crypto = require('crypto')
 
 const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
     modulusLength: 2048,
