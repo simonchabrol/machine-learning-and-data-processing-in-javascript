fc7419b24cd5872a9736f10114947eff269710b6
Simon CHABROL <12834916+simonchabrol@users.noreply.github.com>
2023-09-18 17:16:16 +0200

    Update server.js

--- long_polling/server.js
+++ long_polling/server.js
@@ -4,7 +4,8 @@ var NumClient = 0
 var Previous = 0
 var Responses = []
 
-http.createServer(function(req, res) {      
+http.createServer(function(req, res) {  
+    var Current = 0    
     if (req.method === 'GET' && req.url === '/') {           
        NumClient += 1           
        res.writeHead(200, {                
@@ -13,9 +14,15 @@ http.createServer(function(req, res) {
        Responses.push(res)        
        if (NumClient > Previous) {                      
           for (var i = 0; i < Responses.length; i++) {               
-             Responses[i].write('<br> Client '+NumClient+' is connected<br>')   
+             Responses[i].write('<br> Client '+NumClient+' is connected<br>')  
+             Current = NumClient 
           }   
           Previous = NumClient     
        }   
     }
+    req.on('close',function(){    
+      for (var i = 0; i < Responses.length; i++) {               
+         Responses[i].write('<br> Client '+Current+' is disconnected<br>')  
+      }   
+   })
 }).listen(4040)
